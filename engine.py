9e69a72a1a6367de05eb6ca2419cd6212ca4f496
wnelson <wnelson11@gmail.com>
2022-02-26 15:54:16 -0700

    add backward pass

--- engine.py
+++ engine.py
@@ -1,8 +1,6 @@
 import numpy as np
 import tqdm
 
-import turbulence
-
 #==============================================================================
 #==============================================================================
 class atm_channel:
@@ -13,8 +11,21 @@ class atm_channel:
             screen = turb.get_phase_screen(self.screen_distance)
             self.phase_screens.append(screen)
 
-    def forward(self, beam):
-        for phase_screen in tqdm.tqdm(self.phase_screens):
+    def forward(self, beam, progress_bar=False):
+        self.__single_pass(
+            beam,
+            self.phase_screens,
+            progress_bar)
+
+    def backward(self, beam, progress_bar=False):
+        self.__single_pass(
+            beam,
+            self.phase_screens.reverse(),
+            progress_bar)
+
+    def __single_pass(self, beam, phase_screens, progress_bar):
+        disable_pbar = not progress_bar
+        for phase_screen in tqdm.tqdm(phase_screens, disable=disable_pbar):
             beam.propagate(self.screen_distance / 2.0)
             beam.distort(phase_screen)
             beam.propagate(self.screen_distance / 2.0)
