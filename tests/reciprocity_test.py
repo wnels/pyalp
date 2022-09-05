1fe211fdc649f8486298c148fa2412c73eaa7da3
wnelson <wnelson11@gmail.com>
2022-09-05 20:11:53 +0000

    update reciprocity test to use mirror target for simplicity

--- tests/reciprocity_test.py
+++ tests/reciprocity_test.py
@@ -1,12 +1,10 @@
 import argparse
 import numpy as np
-import matplotlib.pyplot as plt
 import tqdm
 import yaml
 
 from pyalp.beams import guassian
-from pyalp.components import atmosphere, lens, phase_screen, reflector, spatial_filter
-from pyalp.diagnostics import display
+from pyalp.components import atmosphere, lens, phase_screen, spatial_filter
 from pyalp.domain import grids
 
 #------------------------------------------------------------------------------
@@ -32,17 +30,22 @@ def reciprocity_experiment(config_path, instances, save_interval=10):
 
     for index in tqdm.tqdm(range(instances)):
 
-        turb = phase_screen.kolmogorov(grid, **config['turbulence']['kolmogorov'])
-        channel = atmosphere.channel(turb, **config['turbulence']['atmosphere'])
-        target = reflector.rough(grid)
+        turb = phase_screen.kolmogorov(
+            grid,
+            **config['turbulence']['kolmogorov'])
+
+        channel = atmosphere.channel(
+            turb,
+            **config['turbulence']['atmosphere'])
 
         beam = guassian.gaussian(grid, **config['beam'])
+
         channel.forward(beam)
-        target_values[index] = np.sum(np.square(beam.x_field) * target.get_phasor())
-        target.propagate(beam)
+        target_values[index] = np.sum(np.square(beam.x_field))
         channel.backward(beam)
         gauss_filter.propagate(beam)
         imaging_lens.focus(beam)
+
         detector_values[index] = beam.get_on_axis_field()
 
     return target_values, detector_values
@@ -51,8 +54,11 @@ def reciprocity_experiment(config_path, instances, save_interval=10):
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(
-        description='single pass atmospheric channel simulation')
+    desc = \
+        'test if the integral of target field squared  multipled by the ' \
+        'interaction with target equals the on-axis field at the detector'
+
+    parser = argparse.ArgumentParser(description=desc)
 
     parser.add_argument(
         'config_path',
@@ -67,9 +73,10 @@ if __name__ == '__main__':
         help='number of independent turbulent channels to average')
 
     args = parser.parse_args()
+
     target_values, detector_values = reciprocity_experiment(
         args.config_path,
         args.instances)
 
-    assert np.all(np.isclose(target_values, detector_values, rtol=1e-2)), \
+    assert np.all(np.isclose(target_values, detector_values)), \
         'Optical setup is not reciprocal'
