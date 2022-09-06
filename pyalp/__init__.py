936bfa1412416aa924b9e756e9dcbf542c53c962
wnelson <wnelson11@gmail.com>
2022-09-06 13:24:13 -0600

    simplify imports

---
+++ pyalp/__init__.py
@@ -0,0 +1,12 @@
+from .beams import beams
+
+from .components import adaptive_optics
+from .components import atmosphere
+from .components import lens
+from .components import phase_screen
+from .components import reflector
+from .components import spatial_filter
+
+from .diagnostics import display
+
+from .domain import grids
